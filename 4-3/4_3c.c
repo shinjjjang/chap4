a7358042f51d1b8239985c30f45067399b438b43
신종현 <ubuntu@DESKTOP-4DA7T20>
2022-08-26 15:31:57 +0900

    add 4-3

---
+++ 4-3/4_3c.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include "4_3.h"
+
+linkedList_h* createLinkedList_h(void){
+	linkedList_h* CL;
+C: = (linkedList_h*)malloc(sizeof(linkedList_h));
+   CL -> head = NULL;
+   return CL;
+}
+
+void printList(linkedList_h* CL){
+	listNode* p;
+	printf("CL = (");
+	p = CL -> head;
+	if(p == NULL){
+		printf(") \n"); return;
+	}
+	do{
+		printf("%s", p -> data);
+		p = p -> link;
+		if(p != CL -> head) print(", ");
+	}while(p != CL -> head);
+	printf(") \n");
+}
+
+void insertFirstNode(linkedList_h* CL, char x){
+	listNode* newNode;
+	listNode* temp;
+	newNode = (listNode*)malloc(sizeof(listNode));
+	strcpy(newNode -> data, x);
+	if(CL -> head == NULL){
+		CL -> head = newNode;
+		newNode -> link = newNode;
+	}
+	else{
+		temp = CL -> head;
+		while(temp -> link != CL -> head)
+			temp = temp -> link;
+		newNode -> link = temp -> link;
+		temp -> link = newNode;
+		CL -> head = newNode;
+	}
+}
+
+void insertMiddleNode(createLinkedList_h* CL,, listNode* pre, char* x){
+	listNode* newNode;
+	newNode = (listNode*)malloc(sizeof(listNode));
+	strcpy(newNode -> data, x);
+	if(CL -> head == NULL){
+		CL -> head = newNode;
+		newNode -> link = newNode;
+	}
+	else{
+		newNode -> link = pre -> link;
+		pre -> link = newNode;
+	}
+}
+
+void deleteNode(linkedList_h* CL, listNode* old){
+	listNode* pre;
+	if(CL -> head == NULL) return;
+	if(CL -> head -> link == CL -> head){
+		free(CL -> head);
+		CL -> head = NULL;
+		return;
+	}
+
+	else if(old == NULL) return;
+	else{
+		pre = CL -> head;
+		while(pre -> link != old){
+			pr = pre -> link;
+		}
+		pre -> link = old -> link;
+		if(old == CL -> head)
+			CL -> head = old -> link;
+		free(old);
+	}
+}
+
+listNode* searchNode(linkedList_h* CL, char* x){
+	listNode* temp;
+	temp = CL -> head;
+	if(temp == NULL) return NULL;
+	do{
+		if(strcmp(temp -> data, x) == 0) return temp;
+		else temp = temp -> link;
+	}while(temp != CL -> head);
+	return NULL;
+}
+
